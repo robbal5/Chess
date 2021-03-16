41dc6b3e1ea8df0abbf5e6f9d0b3ab20057e04a6
Robby Balistreri <rbalistreri5@gmail.com>
2021-03-16 13:23:18 -0500

    Initialize board and piece class

---
+++ Chess/board.rb
@@ -0,0 +1,28 @@
+class Board
+
+    def initialize
+        @rows = Array.new(8) {Array.new(8)}
+        @null_piece = nil
+    end
+
+    def [](position)
+        row, col = position
+        @rows[row][col]
+
+    end
+
+    def []=(position, value)
+        row, col = position
+        @rows[row][col] = value
+    end
+
+
+    def move_piece(start_pos, end_pos)
+        raise "Invalid starting position, empty space" if self[start_pos] == nil
+        raise "Invalid end position" if self[start_pos].color == self[end_pos].color
+        self[end_pos] = self[start_pos]
+        self[start_pos] = nil
+            
+    end
+
+end
