78f47600c47585abf8a240bd5ae5d91883d949a3
Robby Balistreri <rbalistreri5@gmail.com>
2021-03-17 10:25:35 -0500

    Refactor grow_unblocked_moves on Slidable module

--- Chess/slideable.rb
+++ Chess/slideable.rb
@@ -1,3 +1,4 @@
+require "byebug"
 module Slideable
   HORIZONTAL_DIRS = [
     [1,0],
@@ -11,17 +12,17 @@ DIAGONAL_DIRS = [
  [-1,-1],
  [-1,1]
 ]
-private_constant :DIAGONAL_DIRS, :HORIZONTAL_DIRS    
-def moves
+private_constant :DIAGONAL_DIRS, :HORIZONTAL_DIRS  
+
+    def moves
       possible_moves = []
-      move_dirs.each do |dir|
-        
-        dir.each do |subD|
+      move_dirs.each do |dir|  
+        # debugger
           
-          dir1, dir2 = subD
+          dir1, dir2 = dir
         possible_moves += grow_unblocked_moves_in_dir(dir1, dir2) 
-      end
-    end  
+      
+      end  
     possible_moves
     end 
 
@@ -42,22 +43,26 @@ def moves
     def grow_unblocked_moves_in_dir(dx, dy)
       current_pos = @pos.dup
       moves = []
-      current_pos[0] += dx
-        current_pos[1] += dy
-      until @board[current_pos] != nil 
-          
-        if current_pos.min >= 0 && current_pos.max < 7 
-          moves << current_pos
-          else
-            break 
-          end
+      finished = false
+      # debugger
+      until finished 
+          current_pos[0] += dx
+          current_pos[1] += dy
 
-        current_pos[0] += dx
-        current_pos[1] += dy
+          break unless current_pos.min >= 0 && current_pos.max <= 7 
+        
+          if @board[current_pos].nil?
+            moves << current_pos.dup
+          else
+             if @color == @board[current_pos].color
+                finished = true
+             else
+                moves << current_pos.dup
+                finished = true
+             end
+          end
       end
-      if @board[current_pos] != nil && @board[current_pos].color != @color
-      moves << current_pos
-      end 
+      # debugger
       moves 
     end 
 
